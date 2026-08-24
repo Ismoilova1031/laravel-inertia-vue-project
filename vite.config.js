e581c062303e5ffd014f9147752410b9a5d34191
internship2026-soliha <internship2026-soliha>
2026-08-24 08:20:20 +0000

    rename app.js to app.ts

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
